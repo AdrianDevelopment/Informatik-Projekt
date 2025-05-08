f8316be7baeef30c24e14a811eac999f2c87ad6b
Tom <tom@mail.de>
2025-05-08 15:58:23 +0200

    Aufräumen

--- src/main/java/InformatikProjekt/Main.java
+++ src/main/java/InformatikProjekt/Main.java
@@ -2,11 +2,6 @@ package InformatikProjekt;
 
 public class Main {
     public static void main(String[] args) {
-            //test03Tom
-            //halli jalloo 2
-    }
-
-    void test() {
 
     }
 }
