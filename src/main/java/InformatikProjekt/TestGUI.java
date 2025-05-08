b457d9648b485a7d3aafd73c3d730691bebac1ef
zzQuarks <timokurz323@gmail.com>
2025-05-08 13:02:07 +0200

    + Gui Test KLasse

---
+++ src/main/java/InformatikProjekt/TestGUI.java
@@ -0,0 +1,19 @@
+package InformatikProjekt;
+import javax.swing.JFrame;
+
+public class TestGUI {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setTitle("Schafkopfen");
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        //frame.setDefaultCloseOperation(frame.DO_NOTHING_ON_CLOSE);
+
+    }
+
+
+
+}
