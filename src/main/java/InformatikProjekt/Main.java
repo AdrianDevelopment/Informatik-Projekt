9d04edde3b8c4dc0a2442c0b5a6833fe1578b5c1
zzQuarks <timokurz323@gmail.com>
2025-05-08 12:12:45 +0200

    Test thiemo 2

--- src/main/java/InformatikProjekt/Main.java
+++ src/main/java/InformatikProjekt/Main.java
@@ -3,7 +3,7 @@ package InformatikProjekt;
 public class Main {
     public static void main(String[] args) {
             //test03Tom
-            //halli jalloo
+            //halli jalloo 2
     }
 
     void test() {
