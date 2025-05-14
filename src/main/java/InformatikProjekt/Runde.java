d1fcbf0eba3d5c8c27ea67600aa3b331113e33d8
AdrianDevelopment <adrian.lindstedt@gmx.de>
2025-05-14 09:24:55 +0200

    Hinzufügen Klasse Karte und Variablen in Runde

--- src/main/java/InformatikProjekt/Runde.java
+++ src/main/java/InformatikProjekt/Runde.java
@@ -1,4 +1,13 @@
 package InformatikProjekt;
 
 public class Runde {
+    private Mitspieler[] spieler;
+    private int amZug;
+    private int ausrufer;
+    private Karte ausgerufen;
+    private int ausruferMitspieler;
+    private int[] punkte;
+    private Karte[] letzterStich;
+    private Karte[] aktuellerStich;
+    private SpielArt höchstesSpiel;
 }
