67c8781fca6596f1f1b4901ca37d60e706d6983f
zzQuarks <timokurz323@gmail.com>
2025-05-15 12:09:16 +0200

    + 2 GUI Klassen nicht bricked

--- src/main/java/InformatikProjekt/SpielGUI.java
+++ src/main/java/InformatikProjekt/SpielGUI.java
@@ -3,12 +3,9 @@ package InformatikProjekt;
 import javax.swing.JFrame;
 //import javax.swing.*;
 
-public static void main(String[] args){
-    JFrame frame = new JFrame();
-    frame.setTitle("Schafkopfen");
-    frame.setSize(500,500);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-    frame.setResizable(false);
-    //frame.setDefaultCloseOperation(frame.DO_NOTHING_ON_CLOSE);
+public class SpielGUI{
+
+    public void spielGuiStart(){
+        // kommt nach der MenüGUI
+    }
 }
