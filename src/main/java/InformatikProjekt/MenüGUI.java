93af652c5bde49b39a6712e579fc9c720606187c
zzQuarks <timokurz323@gmail.com>
2025-05-20 22:13:30 +0200

    Menü GUI Geupdated

--- src/main/java/InformatikProjekt/MenüGUI.java
+++ src/main/java/InformatikProjekt/MenüGUI.java
@@ -1,27 +1,49 @@
 package InformatikProjekt;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenüGUI {
+public class MenüGUI{
 
-    public void guiStart(){
-        JFrame frame = new JFrame("Startmenü");
-        frame.setSize(500, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+    //globale Variablen erstellen
+    JButton playButton;
 
-        JLabel label = new JLabel("Schafkopf");
-        label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        label.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
-        frame.add(label);
-
-        JButton button = new JButton("Spiel starten");
-        button.setAlignmentX(JButton.CENTER_ALIGNMENT);
-        frame.add(button);
-        button.addActionListener(e -> {System.out.println("Spiel gestartet");});
-
+    public int guiStart() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Startmenue");
+        frame.setSize(500, 500); //ersetzt duch frame.pack() !frame.pack() muss am ende stehen!
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.green);
+
+        Border border = BorderFactory.createLineBorder(Color.black);
+
+
+        //Hier werden Labels erstellt:
+        JLabel label1 = new JLabel();
+        label1.setText("Willkommen!");
+        label1.setVerticalAlignment(JLabel.TOP);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        label1.setFont(new Font("My Boli",Font.PLAIN, 40));
+        label1.setBounds(75,0,350,40);
+
+        //Hier werden Buttons erstellt:
+        playButton = new JButton();
+        playButton.addActionListener( e -> System.out.println("hi"));
+        playButton.setBounds(150,50,200,40);
+        playButton.setText("SPIELEN");
+        playButton.setFocusable(false);
+
+        //Hier wird der Frame bearbeitet
+        frame.setLayout(null);
+        frame.add(label1);
+        frame.add(playButton);
+
+
+        return 0;
     }
+
 }
