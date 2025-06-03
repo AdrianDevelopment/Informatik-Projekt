b5ceac7c045fc01fb8167c70e9cb1f0ee22b51b1
AdrianDevelopment <adrian.lindstedt@gmx.de>
2025-06-03 15:31:22 +0200

    Ausprobieren der StatistikGUI

--- src/main/java/InformatikProjekt/StatistikGUI.java
+++ src/main/java/InformatikProjekt/StatistikGUI.java
@@ -4,9 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StatistikGUI {
-
+    
+    private Speicherung speicherung;
+    
     public StatistikGUI() {
-        JFrame Statistik = new JFrame("Statistik");
+        speicherung = new Speicherung();
+        
+        JFrame Statistik = new JFrame("Statistiken");
         Statistik.setSize(500, 500);
         Statistik.setVisible(true);
         Statistik.setDefaultCloseOperation(Statistik.EXIT_ON_CLOSE);
@@ -15,14 +19,21 @@ public class StatistikGUI {
 
         //Labels
         JLabel ueberschriftLabel = new JLabel();
-        ueberschriftLabel.setText("Statistik");
+        ueberschriftLabel.setText("Statistiken");
         ueberschriftLabel.setVerticalAlignment(JLabel.TOP);
         ueberschriftLabel.setHorizontalAlignment(JLabel.CENTER);
         ueberschriftLabel.setFont(new Font("My Boli",Font.PLAIN, 40));
-        ueberschriftLabel.setBounds(75,0,350,40);
+        ueberschriftLabel.setBounds(75,2,350,50);
+
+        JLabel spielerGesamtLabel = new JLabel();
+        spielerGesamtLabel.setText("Gewonnene Spiele: " + speicherung.gewonneneSpieleGeben());
+        spielerGesamtLabel.setBounds(75, 75, 350, 50);
+        spielerGesamtLabel.setHorizontalAlignment(JLabel.CENTER);
+        spielerGesamtLabel.setFont(new Font("My Boli", Font.PLAIN, 20));
 
         //Frame
         Statistik.add(ueberschriftLabel);
+        Statistik.add(spielerGesamtLabel);
 
     }
 
