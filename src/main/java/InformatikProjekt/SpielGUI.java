45fe29adaa957b312a3ce9cf7607c90b24fc6761
zzQuarks <timokurz323@gmail.com>
2025-06-05 23:41:02 +0200

    paar methoden und neue array lists

--- src/main/java/InformatikProjekt/SpielGUI.java
+++ src/main/java/InformatikProjekt/SpielGUI.java
@@ -65,7 +65,6 @@ class SpielGUI {
         mainFrame = new JFrame();
         mainFrame.setSize(1400, 700); //ersetzt duch frame.pack() !frame.pack() muss am ende stehen!
         mainFrame.setVisible(true);
-        //mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(null);
 
         weiterButton = new JRadioButton("Weiter");
