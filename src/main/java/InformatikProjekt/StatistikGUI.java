e89e8c51be677ae8d2b511dca0feda4d17ca6f82
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2025-06-05 12:17:43 +0200

    Tripi tropi tropa tripa
    Tralalero tralala porque die porquallah
    Tung Tung Tung Tung Tung Tung sahur

--- src/main/java/InformatikProjekt/StatistikGUI.java
+++ src/main/java/InformatikProjekt/StatistikGUI.java
@@ -7,6 +7,15 @@ public class StatistikGUI {
 
     private Speicherung speicherung;
 
+    private void TextAnzeigen(JLabel[] labels, int index, String text){
+        JLabel neuesLabel=new JLabel();
+        neuesLabel.setText(text);
+        neuesLabel.setBounds(75,50+(index*29),350,29);
+        neuesLabel.setHorizontalAlignment(JLabel.CENTER);
+        neuesLabel.setFont(new Font("My Boli", Font.PLAIN, 20));
+        labels[index]=neuesLabel;
+    }
+
     public StatistikGUI() {
         speicherung = Speicherung.speicherungErstellen();
 
@@ -25,15 +34,36 @@ public class StatistikGUI {
         ueberschriftLabel.setFont(new Font("My Boli",Font.PLAIN, 40));
         ueberschriftLabel.setBounds(75,2,350,50);
 
-        JLabel spielerGesamtLabel = new JLabel();
+        JLabel[] alleLabels = new JLabel[14];
+
+        TextAnzeigen(alleLabels,0,"Spiele:");
+        TextAnzeigen(alleLabels,1,"Gespielte Spiele: " + speicherung.gespielteSpieleGeben());
+        TextAnzeigen(alleLabels,2,"Gewonnene Spiele: " + speicherung.gewonneneSpieleGeben());
+        TextAnzeigen(alleLabels,3,"Verlorene Spiele: " + speicherung.verloreneSpieleGeben());
+
+        TextAnzeigen(alleLabels,5,"Turniere:");
+        TextAnzeigen(alleLabels,6,"Gespielte Turniere: " + speicherung.gespielteTurniereGeben());
+        TextAnzeigen(alleLabels,7,"Gewonnene Turniere: " + speicherung.gewonneneTurniereGeben());
+        TextAnzeigen(alleLabels,8,"Verlorene Turniere: " + speicherung.verloreneTurniereGeben());
+
+        TextAnzeigen(alleLabels,10,"Sonstige:");
+        TextAnzeigen(alleLabels,11,"Gespielte Karten: " + speicherung.gespielteKartenGeben());
+        TextAnzeigen(alleLabels,12,"Höchste Punktzahl (Runde): " + speicherung.hoechstePunktzahlRundeGeben());
+        TextAnzeigen(alleLabels,13,"Gesamte Punktzahl: " + speicherung.gesamtePunkteGeben());
+
+        /*JLabel spielerGesamtLabel = new JLabel();
         spielerGesamtLabel.setText("Gewonnene Spiele: " + speicherung.gewonneneSpieleGeben());
         spielerGesamtLabel.setBounds(75, 75, 350, 50);
         spielerGesamtLabel.setHorizontalAlignment(JLabel.CENTER);
-        spielerGesamtLabel.setFont(new Font("My Boli", Font.PLAIN, 20));
+        spielerGesamtLabel.setFont(new Font("My Boli", Font.PLAIN, 20));*/
 
         //Frame
         Statistik.add(ueberschriftLabel);
-        Statistik.add(spielerGesamtLabel);
+        //Statistik.add(spielerGesamtLabel);
+        for (int i=0;i<alleLabels.length;++i){
+            if (alleLabels[i]!=null)
+                Statistik.add(alleLabels[i]);
+        }
 
     }
 
