d147a04bccaf5873626478e67a41937d43277bc6
Tom <tom@mail.de>
2025-06-05 13:17:17 +0200

    Commit bevor Threads eingefügt werden

---
+++ src/test/java/SpielerTest.java
@@ -0,0 +1,15 @@
+import InformatikProjekt.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpielerTest {
+    @Test
+    public void saer() {
+         Spieler spieler = new Spieler();
+
+         assertEquals(spieler.spielabsichtFragen(), , );
+    }
+}
