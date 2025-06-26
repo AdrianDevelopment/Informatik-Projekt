29cb2eea87ad30248b5c3c07c5ff8a0c2a11a2eb
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2025-06-26 12:45:08 +0200

    Brr brr patapim

--- src/main/java/View/TurnierGUI.java
+++ src/main/java/View/TurnierGUI.java
@@ -11,27 +11,28 @@ public class TurnierGUI {
     JLabel label;
     JTextField eingabefeld;
     JButton knopf;
-
     public TurnierGUI(Menue menue){
         this.menue = menue;
         frame = new JFrame("Turnier starten");
-        frame.setBounds(0,0,200,100);
+
+        frame.setLayout(null);
+        frame.setBounds(0,0,800,140);
         label = new JLabel("Anzahl der Runden eingeben:");
-        label.setBounds(0,0,100,50);
+        label.setBounds(0,0,300,50);
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setFont(new Font("My Boli", Font.PLAIN, 20));
         label.setVisible(true);
         frame.add(label);
 
         eingabefeld = new JTextField(20);
-        eingabefeld.setBounds(100,0,100,50);
+        eingabefeld.setBounds(300,0,500,50);
         eingabefeld.setVisible(true);
         eingabefeld.setFont(new Font("My Boli", Font.PLAIN, 20));
         label.setHorizontalAlignment(JLabel.LEFT);
         frame.add(eingabefeld);
 
-        knopf = new JButton("OK");
-        knopf.setBounds(0,50,200,50);
+        knopf = new JButton("Turnier starten");
+        knopf.setBounds(0,50,800,50);
         knopf.setVisible(true);
         knopf.setFont(new Font("My Boli", Font.PLAIN, 20));
         knopf.setHorizontalAlignment(JLabel.CENTER);
@@ -46,5 +47,9 @@ public class TurnierGUI {
         }catch (NumberFormatException e){
             return;
         }
+        int anzahlRunden = i.intValue();
+        if (anzahlRunden<1||anzahlRunden>16)return;
+        menue.turnierStarten(anzahlRunden);
+        frame.dispose();
     }
 }
