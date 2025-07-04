03d15440f1166f2d4fd45ec13156ce0d18103a21
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2025-07-04 10:46:58 +0200

    Ich hasse Java

--- src/main/java/View/TurnierHistorieGUI.java
+++ src/main/java/View/TurnierHistorieGUI.java
@@ -2,12 +2,39 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 import Model.Speicherung;
+import Model.TurnierStatistik;
 
 public class TurnierHistorieGUI {
-    TurnierHistorieGUI(){
+    public static void TurnierStatistikAnzeigen(){
         JFrame frame = new JFrame("Turnierhistorie");
-        frame.setLayout(null);
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        ArrayList<TurnierStatistik> statistiken =
+                Model.Speicherung.speicherungErstellen().gebeAlteTurnierstatistiken();
+        String[] turniereTexte = new String[statistiken.size()];
+        for (int i=0;i<turniereTexte.length;++i){
+            String aktuellerText;
+            TurnierStatistik aktuelleStatistik = statistiken.get(i);
+            if (aktuelleStatistik.wurdeGewonnen()){
+                aktuellerText = "Gewonnen";
+            }else{
+                aktuellerText = "Verloren";
+            }
+            aktuellerText += " mit " + aktuelleStatistik.punkteGeben();
+            aktuellerText += " Punkten. ";
+            aktuellerText += aktuelleStatistik.DatumGeben().format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+            aktuellerText += " ";
+            aktuellerText += aktuelleStatistik.ZeitGeben().format(DateTimeFormatter.ofPattern("HH:mm"));
+            turniereTexte[turniereTexte.length - i - 1] = aktuellerText;
+        }
+        JList<String> liste = new JList<>(turniereTexte);
+        liste.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane ScrollLeiste = new JScrollPane(liste);
+        frame.add(ScrollLeiste);
+        frame.setVisible(true);
     }
 }
